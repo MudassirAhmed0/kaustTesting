292a80bd23777569661afad87a7c122602d36720
MudassirAhmed0 <84226929+MudassirAhmed0@users.noreply.github.com>
2022-12-29 15:26:03 +0500

    orign

--- assets/javascript/aosInit.js
+++ assets/javascript/aosInit.js
@@ -1,1 +1,4 @@
 AOS.init({once: true});
+ let heroVideo =document.querySelector('video')
+ heroVideo.src=  window.innerWidth > 768 ?'./assets/videos/mob_video.mp4':'./assets/videos/mob_video.mp4'
+    
